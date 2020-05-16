9ff6ddcb1014f1c415733a8f3e6ec9aeb5573c19
Antonio Zhu <antoniozhu@hotmail.com>
2020-05-16 21:08:04 +0200

    reformatted blackhole.py

--- blackhole.py
+++ blackhole.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
-import configparser 
-# TODO argparser, custom config 
+import configparser
+# TODO argparser, custom config
 import argparse
 import requests
 import os
@@ -16,18 +16,20 @@ logger = logging.getLogger(__name__)
 _agent = "BlackHole"
 _MAX_TORRENT_COUNT = 5
 
-api = None 
-config = None 
-monitor_path = None 
+api = None
+config = None
+monitor_path = None
 crawl_path = None
 
-torrent_list = [] 
+torrent_list = []
 
 payload = {}
 
 # Read API key from config.ini
+
+
 def getConfig():
-    global api, config, monitor_path, crawl_path    
+    global api, config, monitor_path, crawl_path, torrent_list
     config = configparser.ConfigParser()
     config.read("config.ini")
     api = config['Config']['API']
@@ -38,24 +40,18 @@ def getConfig():
 
     if os.path.isfile("torrent_list.txt"):
         f = open('torrent_list.txt', 'r')
-        torrent_list = map( lambda x : int(x)  ,f.readlines() )
+        torrent_list = list(map(lambda x: int(x), f.readlines()))
 
-    payload['apikey'] = api 
+    payload['apikey'] = api
     payload['agent'] = _agent
 
+
 def testAPI():
-    r = requests.get('http://api.alldebrid.com/v4/user', payload )
+    r = requests.get('http://api.alldebrid.com/v4/user', payload)
     return r.json()['status'] == 'success'
 
+
 def poll():
-    # Get count 
-    r = requests.get('https://api.alldebrid.com/v4/magnet/status', payload)
-    r.raise_for_status()
-    response = r.json()
-
-    if response['status'] == 'success':
-        parseMagnets( response['data']['magnets'] )
-
 
     toUpload = {}
     i = 0
@@ -67,38 +63,53 @@ def poll():
             # If you want, you can send strings to be received as files:
 
             # Apparently it's important to upload as an array with index
-            toUpload['files[%d]' % i] = (f,  open(monitor_path + f, 'rb'), 'application/x-bittorrent')
+            toUpload['files[%d]' % i] = (
+                f,  open(monitor_path + f, 'rb'), 'application/x-bittorrent')
             i += 1
 
-    
+    r = requests.post(
+        'https://api.alldebrid.com/v4/magnet/upload/file', params=payload, files=toUpload)
 
-    r = requests.post('https://api.alldebrid.com/v4/magnet/upload/file', params=payload, files=toUpload)
-
-    # Close items 
+    # Close items
     for toClose in toUpload.values():
         toClose[1].close()
 
     response = r.json()
 
     r.raise_for_status()
-    # Logging 
+    # Logging
     # open('dump_python.json', 'w').write(json.dumps(response, indent=4))
 
-    if response['status'] == 'success':    
-    # Extend torrent list by added torrents
+    if response['status'] == 'success':
+        # Extend torrent list by added torrents
         if len(toUpload) > 0:
-            torrent_list.extend(map(lambda x: x['id'],  response['data']['files']))
+            torrent_list.extend(
+                map(lambda x: x['id'],  response['data']['files']))
             for file in response['data']['files']:
                 filename = file['file']
-                if os.path.isfile(monitor_path + filename ):
-                    try: 
-                        shutil.move( monitor_path + filename , monitor_path + "added/" + filename )
+                if os.path.isfile(monitor_path + filename):
+                    try:
+                        shutil.move(monitor_path + filename,
+                                    monitor_path + "added/" + filename)
                     except PermissionError as e:
                         # TODO: Logger
                         logger.error(e)
                         pass
 
-def parseMagnets( magnetList : list ):
+    # Get count
+    r = requests.get('https://api.alldebrid.com/v4/magnet/status', payload)
+    r.raise_for_status()
+    response = r.json()
+
+    if response['status'] == 'success':
+        parseMagnets(response['data']['magnets'])
+
+    # update torrent_list
+    with open('torrent_list.txt', 'w') as f:
+        f.writelines(map(lambda x: str(x) + "\n", torrent_list))
+
+
+def parseMagnets(magnetList: list):
     count = 0
 
     for magnet in magnetList:
@@ -108,20 +119,22 @@ def parseMagnets( magnetList : list ):
             count += 1
 
         # Ready to download
-        elif code == 4:
+        elif code == 4 and magnet['id'] in torrent_list:
             # Create crawljob for jdownloader
-            # https://github.com/cooolinho/jdownloader-folderwatch/blob/master/crawljob-examples/download-archive.crawljob 
-            f = open( "%s/%s.crawljob" % ( crawl_path ,  str(magnet['id']) ), 'w')
+            # https://github.com/cooolinho/jdownloader-folderwatch/blob/master/crawljob-examples/download-archive.crawljob
+            f = open("%s/%s.crawljob" % (crawl_path,  str(magnet['id'])), 'w')
             f.writelines(generateCrawlJob(magnet))
+            torrent_list.remove(magnet['id'])
             f.close()
-
     return count
 
-# Returns list str 
-def generateCrawlJob(magnet : dict):
+# Returns list str
+
+
+def generateCrawlJob(magnet: dict):
     lines = [
         "packageName=%s" % magnet['filename'],
-        # dirty af 
+        # dirty af
         "text=%s" % str(list(map(lambda x: x['link'], magnet['links']))),
         # jDownloader settings
         "enabled=true",
@@ -131,14 +144,16 @@ def generateCrawlJob(magnet : dict):
         # archive settings
         "extractAfterDownload=TRUE"
     ]
-    
-    return map( lambda s : s + "\n", lines )
+
+    return map(lambda s: s + "\n", lines)
+
 
 def start():
     getConfig()
     if not testAPI():
         raise Exception("API key seems to be wrong")
-    
+
     poll()
+
 
 start()
